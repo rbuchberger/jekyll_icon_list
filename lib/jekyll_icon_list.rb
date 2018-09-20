007af71d4b2a1c968d07edcfc4347833dd9b6230
Robert Buchberger <rbuchberger@gmail.com>
2018-09-20 19:42:56 +0200

    basic html builder

--- lib/jekyll_icon_list.rb
+++ lib/jekyll_icon_list.rb
@@ -1,5 +1,6 @@
-require "jekyll_icon_list/version"
+require 'jekyll_icon_list/version'
 require 'jekyll'
+require 'jekyll-inline-svg'
 # Title: Jekyll Icon List
 # Author: Robert Buchberger : robert@robert-buchberger.com
 # Description: Generates lists of icons + labels, useful for things like tag
@@ -8,21 +9,75 @@ require 'jekyll'
 module JekyllIconList
   # Here's the tag!
   class IconList < Liquid::Tag
-    def initialize(tag_name, content, tokens)
+    def initialize(tag_name, raw_args, tokens)
+      @raw_args = raw_args
       super
     end
 
+    def parse_arguments(raw_args, settings)
+      # Format: {% iconlist tag1, tag2, tag3, ul: class="example", li: class="example2" %}
+
+      args_parser = /(?<icons>.+?)(, ul: (?<ul_attributes>.+?))?(, li: (?<li_attributes>.+?))?\z/
+
+      args_hash = args_parser.match raw_args
+      @icons = args_hash[:icons].split(', ')
+
+
+      @ul_attrs = args_hash[:ul_attributes] 
+      @li_attrs = args_hash[:li_attributes]
+
+      if settings['ul_defaults']
+        @ul_attrs += (" " + settings['ul_defaults'])
+      end
+
+      if settings["li_defaults"]
+        @li_attrs += (" " + settings['li_defaults'])
+      end
+
+      @ul_attrs = @ul_attrs.prepend(' ') if @ul_attrs
+      @li_attrs = @li_attrs.prepend(' ') if @li_attrs
+    end
+
+    def generate_image(icon, settings, context)
+      if settings['icon_path']
+        icon = icon.prepend settings['icon_path']
+      end
+
+      file_ext_regex = /\.([a-zA-Z]{1,4})\z/
+      file_ext = file_ext_regex.match(icon)[1]
+
+      if file_ext == 'svg'
+        JekyllInlineSVG.new('svg', icon).render(context)
+      else
+        "<img src=#{icon}>"
+      end
+    end
+
     def render(context)
-      "Your icon list is working!"
+      # Get site settings
+      site = context.registers[:site]
+      # get iconlist settings
+      settings = site.config['icon_list']
+      # get data file
+      icon_data = site.data['icon_list']
+
+      # Parse the argument string and combine it with site defaults
+      parse_arguments(@raw_args, settings)
+
+      list = "<ul#{@ul_attrs}>\n"
+
+      @icons.each do |i|
+        icon = generate_image(icon_data[i]['icon'], settings, context)
+        label = icon_data[i]['label']
+        list += "<li#{@li_attrs}>#{icon}#{label}</li>\n"
+      end
+
+      list += "</ul>\n"
+
     end
   end
+
+
 end
 
 Liquid::Template.register_tag('iconlist', JekyllIconList::IconList)
-
-
-# Steps to accomplish:
-# Parse tag argument (split comma separated string into an array)
-# -- Argument will have a list of technologies, and a set of attributes for the icons and the list.
-# need a way to set default attributes for the list, so we don't have to type it every time
-# build the list.
