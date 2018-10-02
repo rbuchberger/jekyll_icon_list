c48b48186fe66848e66207cd3f07038b779e33cf
Robert Buchberger <rbuchberger@gmail.com>
2018-10-02 16:26:02 +0200

    Conversion to using objective_elements gem

--- jekyll_icon_list.gemspec
+++ jekyll_icon_list.gemspec
@@ -29,5 +29,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
 
   spec.add_dependency 'jekyll-inline-svg'
-  spec.add_dependency 'objective_elements', '~>0.1.0'
+  spec.add_dependency 'objective_elements', '~>0.2.0'
 end
--- lib/jekyll_icon_list.rb
+++ lib/jekyll_icon_list.rb
@@ -14,12 +14,6 @@ module JekyllIconList
   # separated list of names defined in _data/icons.yml. Acceptable commands are
   # --ul, --li, --svg, and --img. Their arguments are inserted into their
   # respective HTML elements upon render.
-  class FixedDoubleTag < DoubleTag
-    def indent
-      '  '
-    end
-  end
-
   class IconList < Liquid::Tag
     def initialize(tag_name, raw_input, tokens)
       @raw_input = raw_input
@@ -45,29 +39,33 @@ module JekyllIconList
     def parse_input
       # raw_input will look something like this:
       # 'item1 item2 item3 --ul attribute="value" --(...)'
+
       @attributes = @icon_list_settings['defaults'].dup || {}
+      # {'ul' => 'class="awesome" (...)', (...)}
 
       raw_input_array = @raw_input.split('--').map { |i| i.strip.split(' ') }
-      # [['item1', 'item2', 'item3'], ['ul', 'attribute="value"'], (...) ]
+      # [['item1', 'item2', 'item3'], ['ul', 'attr="value', 'value2"'],(...)]
 
       @item_shortnames = raw_input_array.shift
+      # ['ul, 'attribute="value1 value2"', (...)]
 
       raw_input_array.each { |a| @attributes[a.shift] = a.join ' ' }
-      @attributes.each_value { |v| v.prepend(' ') }
+      # {'ul' => 'attribute="value1 value2 value3"'}
     end
 
     def build_html(all_items_data)
       list = DoubleTag.new 'ul', attributes: @attributes['ul']
 
       @item_shortnames.each do |n|
-        this_icon_data = all_items_data[n] || {}
+        this_item_data = all_items_data[n] || {}
 
-        icon_location = find_icon n, this_icon_data
+        icon_location = find_icon n, this_item_data
 
-        label = build_label(n, this_icon_data)
+        label = build_label(n, this_item_data)
 
-        list.add_content build_li(this_icon_data, icon_location, label)
+        list.add_content build_li(this_item_data, icon_location, label)
       end
+
       list.to_s
     end
 
@@ -92,38 +90,13 @@ module JekyllIconList
       search_results.first.prepend '/'
     end
 
-    def build_svg(icon_filename)
-      params = icon_filename
-      params << @attributes['svg'] if @attributes['svg']
-      Jekyll::Tags::JekyllInlineSvg.send(
-        :new,
-        'svg',
-        params,
-        @tokens
-      ).render(@context)
-    end
-
-    def build_img(icon_filename)
-      img_attributes = { src: icon_filename }
-      img_attributes.merge @attributes['img'] if @attributes['img']
-      SingleTag.new 'img', attributes: img_attributes
-    end
-
-    def build_image_tag(icon_filename)
-      if icon_filename.split('.').pop.casecmp('svg').zero?
-        build_svg(icon_filename)
-      else
-        build_img(icon_filename)
-      end
-    end
-
     def build_label(shortname, this_item_data)
       this_item_data['label'] ||
         shortname.split(/[-_]/).map(&:capitalize).join(' ')
     end
 
     def build_li(this_item_data, icon_location, label)
-      li = FixedDoubleTag.new(
+      li = DoubleTag.new(
         'li',
         attributes: @attributes['li'],
         content: [build_image_tag(icon_location), label],
@@ -134,13 +107,38 @@ module JekyllIconList
       li.reset_content build_anchor(this_item_data['url'], li.content)
     end
 
+    def build_image_tag(icon_filename)
+      if icon_filename.split('.').pop.casecmp('svg').zero?
+        build_svg(icon_filename)
+      else
+        build_img(icon_filename)
+      end
+    end
+
+    def build_svg(icon_filename)
+      params = icon_filename
+      params << ' ' + @attributes['svg'] if @attributes['svg']
+      Jekyll::Tags::JekyllInlineSvg.send(
+        :new,
+        'svg',
+        params,
+        @tokens
+      ).render(@context)
+    end
+
+    def build_img(icon_filename)
+      img = SingleTag.new 'img', attributes: { src: icon_filename }
+      img.add_attributes @attributes['img'] if @attributes['img']
+    end
+
     def build_anchor(url, content)
-      li.add_parent DoubleTag.new(
+      a = DoubleTag.new(
         'a',
-        attributes: @attributes['a'].merge(href: url),
+        attributes: { href: url },
         oneline: true,
         content: content
       )
+      a.add_attributes @attributes['a'] if @attributes['a']
     end
   end
 end
